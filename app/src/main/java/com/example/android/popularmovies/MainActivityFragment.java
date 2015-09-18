a207fb27f958624bff8a1be2e4493aa54f064842
gonzajaimes <gonzajaimes@hotmail.com>
2015-09-17 22:00:47 -0500

    add ImageAdapter

--- app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
@@ -1,15 +1,21 @@
 package com.example.android.popularmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ImageAdapter mPosterAdapter;
+
 
     public MainActivityFragment() {
     }
@@ -17,10 +23,27 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mPosterAdapter = new ImageAdapter(getActivity());
 
 
+        // get reference to the grid view and use the Image Adapter to populate it
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        GridView gridview = (GridView) rootView.findViewById(R.id.gridview);
+        gridview.setAdapter(mPosterAdapter);
+
+        // show a toast with the image position when each image is clicked
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getActivity(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
 
 
     }
