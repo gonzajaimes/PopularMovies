22b5b0e3bb0182c2c80c16495a81f73ead3cae8b
gonzajaimes <gonzajaimes@hotmail.com>
2015-09-25 18:25:46 -0500

    Load Poster images to the grid

--- app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.popularmovies;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,19 +15,24 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    private ImageAdapter mPosterAdapter;
+    private MoviesAdapter mPosterAdapter;
 
 
     public MainActivityFragment() {
@@ -63,7 +69,7 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        mPosterAdapter = new ImageAdapter(getActivity());
+        mPosterAdapter = new MoviesAdapter(getActivity(), new ArrayList<Movie>());
 
 
         // get reference to the grid view and use the Image Adapter to populate it
@@ -85,19 +91,94 @@ public class MainActivityFragment extends Fragment {
 
 
     }
+    private void updateMovies() {
+        FetchMoviesTask moviesTask = new FetchMoviesTask();
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        //String location = prefs.getString(getString(R.string.pref_location_key),
+         //       getString(R.string.pref_location_default));
+        moviesTask.execute();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateMovies();
+    }
 
 
 
-    public class FetchMoviesTask extends AsyncTask<Void, Void, Void> {
+    public class FetchMoviesTask extends AsyncTask<Void, Void, ArrayList<Movie>> {
 
         private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
 
+        /**
+         * Take the String representing the complete forecast in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         *
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private ArrayList<Movie> getMoviesDataFromJson(String moviesJsonStr)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String MDB_RESULTS = "results";
+            final String MDB_ID = "id";
+            final String MDB_POSTER_PATH = "poster_path";
+            ArrayList<Movie> moviesCollection = new ArrayList<Movie>();
+
+            JSONObject moviesJson = new JSONObject(moviesJsonStr);
+            JSONArray moviesArray = moviesJson.getJSONArray(MDB_RESULTS);
+
+            // MDB returns movies information based on the type of query made to the API
+
+
+
+           // String[] resultStrs = new String[moviesArray.length()];
+
+
+
+            for(int i = 0; i < moviesArray.length(); i++) {
+
+                String idMovie;
+                String posterPath;
+
+
+                // Get the JSON object representing the movie
+                JSONObject movieInfo = moviesArray.getJSONObject(i);
+
+
+                // get the ID of the movie.
+                idMovie = movieInfo.getString(MDB_ID);
+
+                // get the poster URL Path
+
+                posterPath = movieInfo.getString(MDB_POSTER_PATH);
+
+
+                // Add the movie to the Movies Array
+                moviesCollection.add(new Movie(idMovie,posterPath));
+
+
+
+
+               // resultStrs[i] = idMovie + " - " + posterPath;
+               // Log.v(LOG_TAG, "movies reported " + resultStrs[i]);
+            }
+
+            return moviesCollection;
+
+        }
+
         @Override
-        protected Void doInBackground(Void... params) {
+        protected ArrayList<Movie> doInBackground(Void... params) {
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
+            String sortBy = "popularity.desc";
+            String key = "b2b68d438d36572bfca895dd822e6caa";
+
 
             // Will contain the raw JSON response as a string.
             String moviesJsonStr = null;
@@ -106,7 +187,23 @@ public class MainActivityFragment extends Fragment {
                 // Construct the URL for the MovieDB query
                 // Possible parameters are avaiable at MDB's forecast API page, at
                 // https://www.themoviedb.org
-                URL url = new URL("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=[your key]");
+
+                final String FORECAST_BASE_URL =
+                        "http://api.themoviedb.org/3/discover/movie?";
+                final String SORT_BY_PARAM = "sort_by";
+                final String KEY_PARAM = "api_key";
+
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(SORT_BY_PARAM, sortBy)
+                        .appendQueryParameter(KEY_PARAM, key)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
+
+
 
                 // Create the request to TheMovieDB, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -136,10 +233,10 @@ public class MainActivityFragment extends Fragment {
                 }
                 moviesJsonStr = buffer.toString();
 
-                Log.v(LOG_TAG, "Movies JSON String: " + moviesJsonStr );
+                //Log.v(LOG_TAG, "Movies JSON String: " + moviesJsonStr);
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attemping
+                // If the code didn't successfully get the movie data, there's no point in attemping
                 // to parse it.
                 return null;
             } finally {
@@ -154,8 +251,31 @@ public class MainActivityFragment extends Fragment {
                     }
                 }
             }
+
+            try {
+                return getMoviesDataFromJson(moviesJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
             return null;
         }
+
+
+        @Override
+        protected void onPostExecute(ArrayList<Movie> result) {
+            if (result != null) {
+                mPosterAdapter.clear();
+                for(Movie movie : result) {
+                    mPosterAdapter.add(movie);
+                }
+
+            }
+        }
+
+
+
     }
 
 
